e966ebb834512d9b0cd6049e9790e0cc80436128
HamzhSuilik <9hamzh@gmail.com>
2021-05-04 05:52:30 +0300

    class1

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,13 @@
 from linked_list.linked_list import LinkedList
 
+from challenges.array_reverse.array_reverse import reverseArray
+
 
 def test_import():
     assert LinkedList
+
+
+
+def test2 () :
+    assert True
+
