77c05a634a574f2f36f266aed74a484e830c2570
HamzhSuilik <9hamzh@gmail.com>
2021-06-17 01:18:06 +0300

    ch-27

---
+++ python/tests/test_merge_sort.py
@@ -0,0 +1,24 @@
+from challenges.merge_sort.merge_sort import merge_sort
+
+def test_sort_1():
+    arr = [2,1,99,88,100,14,5]
+    arr = merge_sort(arr)
+    actual = arr
+    expected = [1, 2, 5, 14, 88, 99, 100]
+    assert actual == expected
+
+def test_sort_2():
+    arr = [0,9,7,3,1]
+    arr = merge_sort(arr)
+    actual = arr
+    expected = [0, 1, 3, 7, 9]
+    assert actual == expected
+
+
+# edge case empty array
+def test_sort_3():
+    arr = []
+    arr = merge_sort(arr)
+    actual = arr
+    expected = []
+    assert actual == expected
