de4a4c4f3b1351e9b7482c6bc491337c9c938ac8
HamzhSuilik <9hamzh@gmail.com>
2021-06-17 23:55:26 +0300

    class-30

---
+++ python/tests/test_hashtable.py
@@ -0,0 +1,13 @@
+from hashtable.hashtable  import *
+
+# Adding a key/value to your hashtable results in the value being in the data structure
+# Retrieving based on a key returns the value stored
+# Successfully returns null for a key that does not exist in the hashtable
+# Successfully handle a collision within the hashtable
+# Successfully retrieve a value from a bucket within the hashtable that has a collision
+# Successfully hash a key to an in-range value
+
+def test_num():
+    actual = 5*1
+    expected = 5
+    assert actual == expected
